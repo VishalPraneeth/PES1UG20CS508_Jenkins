58c4d51a194ec7d975694e441d7cdbb5ca236c9c
Vishal <vishalpraneeth7771@gmail.com>
2023-02-19 21:36:57 +0530

    renamed

--- main/PES1UG20CS508.cpp
+++ main/PES1UG20CS508.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int main(void) {
-  cout << "Hello, EKANATHREDDY\n";
+  cout << "Hello, Vishal M\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
   cout << "PES1UG20CS508\n";
